8661fa188e97547c622106c411b1ede94a9c82b9
Luan A. <barkon32@tuta.io>
2024-02-11 14:57:41 -0300

    feat: initial commit

---
+++ src/main/kotlin/br/com/luan/creditrequestsystem/CreditRequestSystemApplication.kt
@@ -0,0 +1,11 @@
+package br.com.luan.creditrequestsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditRequestSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditRequestSystemApplication>(*args)
+}
