8eb3b532c2982efbd3da5ecabae56ed8d9d3bcb9
Luan A. <barkon32@tuta.io>
2024-02-17 16:47:03 -0300

    feat: create Customer resources

---
+++ src/main/kotlin/br/com/luan/creditrequestsystem/controller/CustomerResource.kt
@@ -0,0 +1,42 @@
+package br.com.luan.creditrequestsystem.controller
+
+import br.com.luan.creditrequestsystem.dto.CustomerDto
+import br.com.luan.creditrequestsystem.dto.CustomerUpdateDto
+import br.com.luan.creditrequestsystem.dto.CustomerView
+import br.com.luan.creditrequestsystem.service.imp.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerResource(
+    private val customerService: CustomerService
+) {
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<String> {
+        val savedCustomer = this.customerService.save(customerDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${savedCustomer.email} saved.")
+    }
+
+    @GetMapping("/{id}")
+    fun findById(id: String): ResponseEntity<CustomerView> {
+        val customer = this.customerService.findById(id)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customer))
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: String) =
+        this.customerService.delete(id)
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: String,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): ResponseEntity<CustomerView> {
+        val customer = this.customerService.findById(id)
+        val customerToUpdate = customerUpdateDto.toEntity(customer)
+        val customerUpdated = this.customerService.save(customerToUpdate)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customerUpdated))
+    }
+}
