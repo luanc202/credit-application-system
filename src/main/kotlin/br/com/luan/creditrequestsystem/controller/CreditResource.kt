70de0d3cad439c2ae22f5178e101686d633bb96f
Luan A. <barkon32@tuta.io>
2024-02-17 16:46:56 -0300

    feat: create Credit resources

---
+++ src/main/kotlin/br/com/luan/creditrequestsystem/controller/CreditResource.kt
@@ -0,0 +1,38 @@
+package br.com.luan.creditrequestsystem.controller
+
+import br.com.luan.creditrequestsystem.dto.CreditDto
+import br.com.luan.creditrequestsystem.dto.CreditView
+import br.com.luan.creditrequestsystem.dto.CreditViewList
+import br.com.luan.creditrequestsystem.entity.Credit
+import br.com.luan.creditrequestsystem.service.imp.CreditService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
+import java.util.stream.Collectors
+
+@RestController
+@RequestMapping("/api/credits")
+class CreditResource(
+    private val creditService: CreditService
+) {
+    @PostMapping
+    fun saveCredit(@RequestBody creditDto: CreditDto): ResponseEntity<String> {
+        val credit: Credit = this.creditService.save(creditDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED).body("Credit ${credit.creditCode} of Customer ${credit.customer?.firstName} saved")
+    }
+
+    @GetMapping
+    fun findAllByCustomer(@RequestParam(value = "customerId") customerId: String): ResponseEntity<List<CreditViewList>> {
+        val creditViewList = this.creditService.findAllByCustomer(customerId).stream()
+            .map { credit: Credit -> CreditViewList(credit) }
+            .collect(Collectors.toList())
+        return ResponseEntity.status(HttpStatus.OK).body(creditViewList)
+    }
+
+    @GetMapping("/{creditCode}")
+    fun findByCreditCode(@RequestParam(value = "customerId") customerId: String, @PathVariable creditCode: UUID): ResponseEntity<CreditView> {
+        val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+        return ResponseEntity.status(HttpStatus.OK).body(CreditView(credit))
+    }
+}
