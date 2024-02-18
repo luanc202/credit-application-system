2678047d49bfe7a6ca82e2639d0dfc7790285c07
Luan A. <barkon32@tuta.io>
2024-02-18 15:56:42 -0300

    feat: add constraint of a max of 48 to installments number

--- src/main/kotlin/br/com/luan/creditrequestsystem/dto/CreditDto.kt
+++ src/main/kotlin/br/com/luan/creditrequestsystem/dto/CreditDto.kt
@@ -3,6 +3,7 @@ package br.com.luan.creditrequestsystem.dto
 import br.com.luan.creditrequestsystem.entity.Credit
 import br.com.luan.creditrequestsystem.entity.Customer
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
@@ -12,7 +13,7 @@ import java.time.LocalDate
 data class CreditDto (
     @field:NotNull(message = "Credit value must not be null") val creditValue: BigDecimal,
     @field:Future(message = "First day of installment must be in the future") val dayFirstOfInstallment: LocalDate,
-    @field:NotNull @field:Positive(message = "Number of installments must be at least 1 or greater integer") val numberOfInstallments: Int,
+    @field:NotNull @field:Max(48, message = "Maximum number of installments is 48.") @field:Positive(message = "Number of installments must be at least 1 or greater integer") val numberOfInstallments: Int,
     @field:NotEmpty(message = "Customer id must not be empty") val customerId: String
 ) {
     fun toEntity(): Credit = Credit (
