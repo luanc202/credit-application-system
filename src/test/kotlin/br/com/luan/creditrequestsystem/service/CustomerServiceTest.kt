bae907f5568770adfe01166044da9228651d0ce1
Luan A. <barkon32@tuta.io>
2024-02-20 20:33:54 -0300

    feat: created test for delete a customer

--- src/test/kotlin/br/com/luan/creditrequestsystem/service/CustomerServiceTest.kt
+++ src/test/kotlin/br/com/luan/creditrequestsystem/service/CustomerServiceTest.kt
@@ -10,6 +10,8 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
@@ -61,6 +63,19 @@ class CustomerServiceTest {
             .isThrownBy { customerService.findById(fakeId) }
             .withMessage("Id $fakeId not found")
         verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should delete a customer by id` () {
+        val fakeId: String = UUID.randomUUID().toString();
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.delete(fakeCustomer) } just runs
+
+        customerService.delete(fakeId)
+
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.delete(fakeCustomer) }
     }
 
     private fun buildCustomer(
