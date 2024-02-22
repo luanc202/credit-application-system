8ea214a0a8363f5b9524fe94b9ceecc2771146e4
Luan A. <barkon32@tuta.io>
2024-02-22 10:10:57 -0300

    feat: updated id type

--- src/test/kotlin/br/com/luan/creditrequestsystem/service/CustomerServiceTest.kt
+++ src/test/kotlin/br/com/luan/creditrequestsystem/service/CustomerServiceTest.kt
@@ -3,7 +3,6 @@ package br.com.luan.creditrequestsystem.service
 import br.com.luan.creditrequestsystem.entity.Address
 import br.com.luan.creditrequestsystem.entity.Customer
 import br.com.luan.creditrequestsystem.exception.BusinessException
-import br.com.luan.creditrequestsystem.repository.CreditRepository
 import br.com.luan.creditrequestsystem.repository.CustomerRepository
 import br.com.luan.creditrequestsystem.service.imp.CustomerService
 import io.mockk.every
@@ -42,39 +41,39 @@ class CustomerServiceTest {
 
     @Test
     fun `should find customer by id` () {
-        val fakeId: String = UUID.randomUUID().toString();
+        val fakeId: UUID = UUID.randomUUID();
         val fakeCustomer: Customer = buildCustomer(id = fakeId)
-        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.findById(fakeId.toString()) } returns Optional.of(fakeCustomer)
 
         val actual: Customer = customerService.findById(fakeId)
         Assertions.assertThat(actual).isNotNull
         Assertions.assertThat(actual).isSameAs(fakeCustomer)
         Assertions.assertThat(actual).isExactlyInstanceOf(Customer::class.java)
-        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.findById(fakeId.toString()) }
     }
 
     @Test
     fun `should not find customer by invalid id and throw BusinessException` () {
-        val fakeId: String = UUID.randomUUID().toString();
+        val fakeId: UUID = UUID.randomUUID();
 
-        every { customerRepository.findById(fakeId) } returns Optional.empty()
+        every { customerRepository.findById(fakeId.toString()) } returns Optional.empty()
 
         Assertions.assertThatExceptionOfType(BusinessException::class.java)
             .isThrownBy { customerService.findById(fakeId) }
             .withMessage("Id $fakeId not found")
-        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.findById(fakeId.toString()) }
     }
 
     @Test
     fun `should delete a customer by id` () {
-        val fakeId: String = UUID.randomUUID().toString();
+        val fakeId: UUID = UUID.randomUUID();
         val fakeCustomer: Customer = buildCustomer(id = fakeId)
-        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.findById(fakeId.toString()) } returns Optional.of(fakeCustomer)
         every { customerRepository.delete(fakeCustomer) } just runs
 
         customerService.delete(fakeId)
 
-        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.findById(fakeId.toString()) }
         verify(exactly = 1) { customerRepository.delete(fakeCustomer) }
     }
 
@@ -87,7 +86,7 @@ class CustomerServiceTest {
         zipCode: String = "12345678",
         street: String = "Rua do Lado, Av. Norte",
         income: BigDecimal = BigDecimal.valueOf(1000.0),
-        id: String = UUID.randomUUID().toString()
+        id: UUID = UUID.randomUUID()
     ) = Customer(
         firstName = firstName,
         lastName = lastName,
